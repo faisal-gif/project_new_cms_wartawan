981c3160b11827ba16ce4524ba4588ee3c6e78d2
faisal-gif <fagdera@gmail.com>
2026-07-05 18:54:28 +0700

    fix: update tag validation rules to enforce maximum limits and improve error messages

--- app/Http/Requests/NewsFormRequest.php
+++ app/Http/Requests/NewsFormRequest.php
@@ -24,8 +24,8 @@ class NewsFormRequest extends FormRequest
     {
         return [
             'title'         => 'required|string|max:255',
-            'tag'           => 'required|array|min:1',
-            'tag.*'         => 'string',
+            'tag'                   => 'required|array|min:1|max:3',
+            'tag.*'                 => 'string|max:255',
             'content'       => 'required|string',
 
             // Validasi Gambar
@@ -45,6 +45,8 @@ class NewsFormRequest extends FormRequest
             'image_thumbnail.required'   => 'Gambar Thumbnail wajib diunggah.',
             'image_thumbnail.image'      => 'File harus berupa gambar.',
             'tag.required'       => 'Minimal masukkan satu tag.',
+            'tag.max'                    => 'Maksimal hanya boleh mengirimkan 3 tag.',
+            'tag.*.max'                  => 'Panjang setiap tag tidak boleh lebih dari 255 karakter.',
             'content.required'   => 'Konten berita Wajib diisi',
             'image_caption.required' => 'Caption gambar harus diisi.',
         ];
